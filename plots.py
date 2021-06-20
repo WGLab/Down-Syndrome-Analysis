6e0352348fd411c3217366d5d3e6d064fc72addb
Jim Havrilla <havrillaj@reslnvvhpc062.research.chop.edu>
2021-06-20 06:30:04 -0400

    changes to scripts

--- plots.py
+++ plots.py
@@ -1,13 +1,10 @@
 import urllib.request
 import shutil
-import os
 import pandas as pd
 import numpy as np
-import networkx
 import obonet
 from matplotlib import pyplot as plt
 import argparse
-import csv
 import seaborn as sns
 
 parser = argparse.ArgumentParser(description='Make plots for frequency and age data')
@@ -49,18 +46,37 @@ controlterms = pd.read_csv(args.termdata[1], sep='\t', header=0, index_col=0)
 # 4209 - 115(22q) = 4094 DS patients, 7845 control patients (or could get patient inputs from -n if you want)
 case_n = 4094
 control_n = 7845
+case_doc = 87276
+control_doc = 19494
 if args.numbers:
     case_n = int(args.numbers[0])
     control_n = int(args.numbers[1])
 
 # OR = DeHn/DnHe
 # Case PC = de; Control PC = he; dn = case_n - de; hn = control_n - he
-de = caseterms['PC']
-he = controlterms['PC']
-dn = case_n - de
-hn = control_n - he
+# de = caseterms['PC']
+# he = controlterms['PC']
+de = caseterms['DC']
+he = controlterms['DC']
+
+# dn = case_n - de
+# hn = control_n - he
+dn = case_doc - de
+hn = control_doc - he
 odds = de.mul(hn, fill_value=0)/dn.mul(he, fill_value=1) # in case denom is 0
 odds.sort_values(axis=0, inplace=True, ascending=False)
+#((a+b)!(c+d)!(a+c)!(b+d)!)/a!b!c!d!n!
+fp=pd.concat([de,he,dn,hn], axis=1) # abcd
+fp.columns=['de', 'he', 'dn', 'hn']
+fp=fp.fillna(0)
+import scipy.stats as ss
+import fisher
+print(fp.shape[0])
+# fp['fisher'] = fp.apply(lambda row : ss.fisher_exact([[row['de'], row['he']], [row['dn'], row['hn']]]), axis=1)
+fish = fp.apply(lambda row : fisher.pvalue(row['de'], row['he'], row['dn'], row['hn']).two_tail*fp.shape[0], axis=1) # fisher, and bonferroni correction
+fish.columns=['fisher']
+
+
 
 # if args.filter:
     # controlset = set()
@@ -72,22 +88,25 @@ odds.sort_values(axis=0, inplace=True, ascending=False)
                 # controlset.add(fields[0]) # index, HPO term name
     # df = df.loc[df.index.difference(controlset)]
 
-# odds ratio of terms plot
-fig, ax = plt.subplots()
-sns.set_style('ticks')
-sns.despine(right=True,top=True)
-odds.head(20).plot(kind='bar')
-ax.set_ylabel('Odds Ratio, Patient Frequency')
-from matplotlib import rcParams
-rcParams.update({'figure.autolayout': True})
-plt.savefig(args.output[0]+'.png', bbox_inches='tight')
-plt.close()
+# # odds ratio of terms plot
+# fig, ax = plt.subplots()
+# sns.set_style('ticks')
+# sns.despine(right=True,top=True)
+# odds.head(20).plot(kind='bar')
+# ax.set_ylabel('Odds Ratio, Patient Frequency')
+# from matplotlib import rcParams
+# rcParams.update({'figure.autolayout': True})
+# plt.savefig(args.output[0]+'.png', bbox_inches='tight')
+# plt.close()
 
 # individual term age plots
-odds.to_csv(args.output[0]+'.tsv', sep='\t')
+tsv=pd.concat([odds,fish], axis=1)
+tsv.columns=['OR', 'p-value']
+tsv.to_csv(args.output[0]+'.tsv', sep='\t')
+# odds.to_csv(args.output[0]+'.tsv', sep='\t')
 fig, ax = plt.subplots()
 # fig, ax = plt.subplots(3,2)
-term = 'Abnormal ear physiology'
+term = 'Proptosis'
 # ax.hist(agedict[term], bins, histtype='stepfilled')
 sns.histplot(agedict[term], bins=bins)
 # use top 100 odds ratio terms, plot age dists, heatmap line by line
@@ -102,10 +121,6 @@ plt.xlabel('Age')
 plt.ylabel('Count of Visits')
 plt.title(term)
 print(ticks)
-
-topkeys = odds.head(100).index
-topage = { key: agedict[key] for key in top100 }
-
 # ax[0, 0].plot(agedict['Zonular cataract'])
 sns.set_style('ticks')
 sns.despine(right=True,top=True)
@@ -113,14 +128,24 @@ sns.despine(right=True,top=True)
     # a.set(xlabel='Age', ylabel='Count of Visits')
 plt.savefig(args.output[1]+'.png', bbox_inches='tight')
 plt.close()
+
 # heatmap of top 100 HPO terms by age
 f, ax = plt.subplots(figsize=(15, 15))
 # df = pd.DataFrame.from_dict(agedict,orient='index').transpose()
+topkeys = odds.head(100)
+topage = { key: agedict[key] for key in top100 }
 df = pd.concat({k: pd.Series(v).value_counts() for k, v in topage.items()}, axis=1).transpose()
 print(df)
-ax = sns.heatmap(df, mask=df.isnull(), cmap="icefire", yticklabels=True, cbar_kws={'label': 'Number of Visits'})#, xticklabels=True)
+from matplotlib import colors
+maxvisit=df.max(1).max(0)
+meanvisit=df.max(1).mean(0)
+print(meanvisit, maxvisit)
+norm=colors.TwoSlopeNorm(vmin=0., vcenter=meanvisit, vmax=maxvisit)
+# ax = sns.heatmap(df, mask=df.isnull(), cmap="icefire", yticklabels=True, cbar_kws={'label': 'Number of Visits'})#, xticklabels=True)
+ax = sns.heatmap(df, mask=df.isnull(), cmap="nipy_spectral_r", yticklabels=True, norm=norm, cbar_kws={'label': 'Number of Visits'})#, xticklabels=True)
 ax.set_ylabel("HPO terms", fontsize=16)
 ax.set_xlabel("Age in Years", fontsize=16)
+ax.set_xlim(0,50)
 ax.figure.axes[-1].yaxis.label.set_size(16)
 plt.savefig(args.output[1]+'top100.png', bbox_inches='tight')
 plt.close()
